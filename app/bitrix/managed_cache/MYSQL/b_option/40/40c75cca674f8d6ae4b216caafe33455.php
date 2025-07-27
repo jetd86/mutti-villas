9044664acae83a5eda4b78b8f67f91c00ca29ad1
Sergeev Dima <web.sergeev.dima@gmail.com>
2025-07-27 23:03:39 +0300

    Изменил подключение скриптов

--- app/bitrix/managed_cache/MYSQL/b_option/40/40c75cca674f8d6ae4b216caafe33455.php
+++ app/bitrix/managed_cache/MYSQL/b_option/40/40c75cca674f8d6ae4b216caafe33455.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001753642953';
-$dateexpire = '001753646553';
+$datecreate = '001753646603';
+$dateexpire = '001753650203';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:1:"-";a:1:{s:24:"SkipNonPublicCustomTypes";s:1:"Y";}}}';
 return true;
 ?>
--- app/bitrix/managed_cache/MYSQL/b_option/66/663c0b1ce5c397ac48c08a6b010307f6.php
+++ app/bitrix/managed_cache/MYSQL/b_option/66/663c0b1ce5c397ac48c08a6b010307f6.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001753642953';
-$dateexpire = '001753646553';
+$datecreate = '001753646603';
+$dateexpire = '001753650203';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:1:"-";a:2:{s:18:"entity_iblock_type";s:11:"rest_entity";s:11:"server_path";s:5:"/rest";}}}';
 return true;
 ?>
