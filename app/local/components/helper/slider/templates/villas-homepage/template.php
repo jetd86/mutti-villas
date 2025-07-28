c3a5088f939c65ba9df54a1fb53b89146eed42f9
Sergeev Dima <web.sergeev.dima@gmail.com>
2025-07-28 08:11:58 +0300

    Добавил текст для ссылок

--- app/local/components/helper/slider/templates/villas-homepage/template.php
+++ app/local/components/helper/slider/templates/villas-homepage/template.php
@@ -22,42 +22,47 @@ $this->setFrameMode(true); ?>
     <div class="section-wrapper swiper-wrapper"><?
         foreach ($arResult['ITEMS'] ?: [] as $element) { ?>
             <div class="swiper-slide">
-                <meta name="format-detection" content="telephone=no">
-                <div class="section-container">
-                    <div class="section-block section-info">
-                        <div class="section-title">
-                            <h2><?= $element['NAME'] ?></h2>
-                        </div>
-                        <div class="section-content">
-                            <?= $element['DESCRIPTION'] ?>
-                        </div>
-                        <div class="section-properties"><?
-                            foreach ($element['PROPERTIES'] ?: [] as $property) { ?>
-                                <div class="section-properties__item">
-                                    <div class="section-properties__title"><?=$property['TITLE']?></div>
-                                    <div class="section-properties__value"><?=$property['VALUE']?></div>
-                                </div>
-                            <? } ?>
-                        </div>
-                        <div class="section-control">
-                            <div class="section-price">
-                                <?= sprintf(
-                                    Loc::getMessage('PRICE_FROM') . ' %s %s',
-                                    number_format($element['PRICE']['AMOUNT'], 0, '', ' '),
-                                    $element['PRICE']['CURRENCY']
-                                ) ?>
-                            </div>
-                        </div>
+            <meta name="format-detection" content="telephone=no">
+            <div class="section-container">
+                <div class="section-block section-info">
+                    <div class="section-title">
+                        <h2><?= $element['NAME'] ?></h2>
                     </div>
-
-                    <div class="section-block section-image" style="background-image: url(<?=$element['RESIZED_SRC']?>)">
-                        <a href="/villas/#<?= $element['CODE'] ?>" class="section-action">
-                            <i class="bi bi-arrow-right-short"></i>
-                        </a>
+                    <div class="section-content">
+                        <?= $element['DESCRIPTION'] ?>
+                    </div>
+                    <div class="section-properties"><?
+                        foreach ($element['PROPERTIES'] ?: [] as $property) { ?>
+                            <div class="section-properties__item">
+                                <div class="section-properties__title"><?= $property['TITLE'] ?></div>
+                                <div class="section-properties__value"><?= $property['VALUE'] ?></div>
+                            </div>
+                        <? } ?>
+                    </div>
+                    <div class="section-control">
+                        <div class="section-price">
+                            <?= sprintf(
+                                Loc::getMessage('PRICE_FROM') . ' %s %s',
+                                number_format($element['PRICE']['AMOUNT'], 0, '', ' '),
+                                $element['PRICE']['CURRENCY']
+                            ) ?>
+                        </div>
                     </div>
                 </div>
+
+                <div class="section-block section-image" style="background-image: url(<?= $element['RESIZED_SRC'] ?>)">
+                    <a
+                        href="/villas/#<?= $element['CODE'] ?>"
+                        class="section-action"
+                        aria-label="Подробнее о вилле <?= ucfirst(str_replace('-', ' ', $element['CODE'])) ?>"
+                        title="Перейти к описанию виллы <?= ucfirst(str_replace('-', ' ', $element['CODE'])) ?>"
+                    >
+                        <i class="bi bi-arrow-right-short"></i>
+                    </a>
+                </div>
+            </div>
             </div><?
-        }?>
+        } ?>
     </div><?
     if ($arParams['PROPERTIES']['BUTTON']): ?>
         <div class="section-navigate">
