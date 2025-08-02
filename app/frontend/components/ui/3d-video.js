69b8ae92c579681e76fd2ade345a583659551512
Sergeev Dima <web.sergeev.dima@gmail.com>
2025-08-03 01:18:39 +0300

    Оптимизировал видео для главной

--- app/frontend/components/ui/3d-video.js
+++ app/frontend/components/ui/3d-video.js
@@ -1,3 +1,42 @@
+export function initSectionBannerVideo() {
+    const video = document.querySelector('.section-banner__video');
+    if (!video) return;
+
+    video.poster = bgBanner2x;
+    video.setAttribute('preload', 'none');
+
+    const source = video.querySelector('source');
+    if (source) {
+        source.removeAttribute('src');
+        source.setAttribute('data-src', '/public/3d-tour.mp4?1748257640');
+    }
+    const fallbackImg = video.querySelector('img');
+    if (fallbackImg) {
+        fallbackImg.src = bgBanner1x;
+        fallbackImg.setAttribute('srcset', `${bgBanner1x} 1x, ${bgBanner2x} 2x`);
+    }
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                if (source && source.dataset.src) {
+                    source.src = source.dataset.src;
+                    video.load();
+                }
+                observer.unobserve(video);
+            }
+        });
+    }, { threshold: 0.25 });
+    observer.observe(video);
+
+    video.addEventListener('loadeddata', () => {
+        const wrapper = video.closest('.section-banner');
+        wrapper?.classList.add('video-loaded');
+    });
+}
+
+
+/*
 import bgBanner1x from '@images/bg-about-banner.png?w=768&format=webp&as=src';
 import bgBanner2x from '@images/bg-about-banner.png?w=1536&format=webp&as=src';
 
@@ -10,14 +49,11 @@ export function initSectionBannerVideo() {
 
     const source = video.querySelector('source');
     if (source) {
-
+        source.src = '/public/3d-tour.mp4?1748257640';
+        video.load();
         setTimeout(() => {
-            source.src = '/public/3d-tour.mp4?1748257640';
-            video.load();
-            setTimeout(() => {
-                video.play().catch(() => {});
-            }, 100);
-        }, 2000)
+            video.play().catch(() => {});
+        }, 100);
     }
 
     // Найдём fallback <img> и установим src
@@ -33,3 +69,4 @@ export function initSectionBannerVideo() {
         wrapper?.classList.add('video-loaded');
     });
 }
+*/
