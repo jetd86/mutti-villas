2195d952376b65966892cc45cb97cbfcd96df7a9
Sergeev Dima <web.sergeev.dima@gmail.com>
2025-08-03 00:59:04 +0300

    Перенёс скрипты

--- app/local/templates/mutti/footer.php
+++ app/local/templates/mutti/footer.php
@@ -28,17 +28,6 @@ use Bitrix\Main\Localization\Loc; ?>
     </footer>
 </div>
 
-
-<?php
-use Bitrix\Main\Page\Asset;
-
-Asset::getInstance()->addJs('https://cdn.jsdelivr.net/npm/glightbox/dist/js/glightbox.min.js', true);
-Asset::getInstance()->addCss('<link rel="preload" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" as="style" onload="this.onload=null;this.rel=\'stylesheet\'">');
-Asset::getInstance()->addCss('https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css');
-?>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js" async></script>
-
 <?$APPLICATION->IncludeComponent('helper:modal', '')?>
 <script>
     document.addEventListener('DOMContentLoaded', () => {
