6ecb42e6b6c7f1d86e736f8e0d8c0e5d832d1af3
Sergeev Dima <web.sergeev.dima@gmail.com>
2025-08-03 00:24:37 +0300

    Исправил стилизацию картинок на главной

--- app/local/php_interface/include/bitrix-manifest-loader.php
+++ app/local/php_interface/include/bitrix-manifest-loader.php
@@ -22,17 +22,21 @@ function loadViteAssets(string $entryName = 'main.js?1', array $options = []): v
 
     if (!isset($manifest[$entryName])) return;
 
+
     $publicPath = '/local/assets/';
     $entry = $manifest[$entryName];
     $asset = Asset::getInstance();
 
+    // CSS
     if (!empty($entry['css'])) {
         foreach ($entry['css'] as $cssFile) {
             $filePath = $publicPath . $cssFile;
             $asset->addString('<link rel="preload" href="' . $filePath . '" as="style" onload="this.onload=null;this.rel=\'stylesheet\'">');
+            $asset->addCss($filePath);
         }
     }
 
+    // JS
     if (!empty($entry['file'])) {
         $scriptPath = $publicPath . $entry['file'];
 
@@ -42,13 +46,15 @@ function loadViteAssets(string $entryName = 'main.js?1', array $options = []): v
 
         $attrs = [];
         $attrs[] = 'src="' . $scriptPath . '"';
-        $attrs[] = 'type="module"';
-        $attrs[] = 'defer';
 
-        if (!empty($options['nomodule'])) {
+        if (!empty($options['type']) && $options['type'] === 'nomodule') {
             $attrs[] = 'nomodule';
+        } else {
+            $attrs[] = 'type="module"';
         }
 
-        $asset->addString('<script ' . implode(' ', $attrs) . '></script>');
+        $attrs[] = 'defer';
+
+        $asset->addString('<script ' . implode(' ', $attrs) . ' defer></script>');
     }
 }
