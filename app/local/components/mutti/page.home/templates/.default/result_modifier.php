5e8029e44d7d84eef3483641d441e8823e0d160b
jetd86 <jetdan86@yandex.ru>
2025-08-12 21:36:33 +0300

    icons

--- app/local/components/mutti/page.home/templates/.default/result_modifier.php
+++ app/local/components/mutti/page.home/templates/.default/result_modifier.php
@@ -2,6 +2,9 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
     die();
 
+use Mutti\Service\Image\ImageService;
+use Mutti\Enum\OptionHomeEnum;
+
 /** @var array $arParams */
 /** @var array $arResult */
 /** @global CMain $APPLICATION */
@@ -13,6 +16,21 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
 /** @var string $templateFolder */
 /** @var string $componentPath */
 /** @var MuttiPageHomeComponent $component */
+
+$this->setFrameMode(true);
+$imageService = new ImageService();
+$component = new MuttiPageHomeComponent();
+
+$icons = $component->getAdvantagesElements();
+$arResult['ITEMS']['ICONS'] = $icons;
+
+
+foreach($icons as $key => $arIcon) {
+    $arResult['ITEMS']['ICONS'][$key]['SRC_48'] = $imageService->getResizedWebpSrc($arIcon['ICON'], ['height' => 48, 'width' => 48]);
+    $arResult['ITEMS']['ICONS'][$key]['SRC_96'] = $imageService->getResizedWebpSrc($arIcon['ICON'], ['height' => 96, 'width' => 96]);
+    $arResult['ITEMS']['ICONS'][$key]['SRC_180'] = $imageService->getResizedWebpSrc($arIcon['ICON'], ['height' => 180, 'width' => 180]);
+    $arResult['ITEMS']['ICONS'][$key]['SRC_300'] = $imageService->getResizedWebpSrc($arIcon['ICON'], ['height' => 300, 'width' => 300]);
+}
 
 $arLocationSection = $arResult['ITEMS']['location'];
 foreach ($arLocationSection['ITEMS'] as $key => $arLocationItem) {
