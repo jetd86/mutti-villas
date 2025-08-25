a221f4416e474eb58c22e7d47690d61825c9e39b
root <root@muttivillas.ru>
2025-08-25 14:32:29 +0200

    policy

---
+++ app/policy/index.php
@@ -0,0 +1,17 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Политика конфиденциальности");
+?><?php
+
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+
+$APPLICATION->SetTitle("Политика конфиденциальности");
+$APPLICATION->SetPageProperty("TITLE", "Политика конфиденциальности");
+?>
+
+<div class="content-container container">
+<p>
+Страница в разработке
+</p>
+</div>
+<?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
