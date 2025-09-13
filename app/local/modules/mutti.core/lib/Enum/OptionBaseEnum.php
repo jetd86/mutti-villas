bade45127f030a720c0e0e1c42317d87dd931e4a
root <root@DESKTOP-2T29VPA.localdomain>
2025-09-13 23:18:40 +0300

    10 links

--- app/local/modules/mutti.core/lib/Enum/OptionBaseEnum.php
+++ app/local/modules/mutti.core/lib/Enum/OptionBaseEnum.php
@@ -44,4 +44,7 @@ enum OptionBaseEnum: string
     case BASE_FIELD_TRADEMARK_SIGN = 'global_field_trademark_message_sign';
     case BASE_HEADER_TRADEMARK_SIGN_FIELD = 'global_field_trademark_message_field';
     case BASE_HEADER_MAINPAGE_LINKS_FIELD = 'global_field_mainpage_links_field';
+    case BASE_HEADER_MAINPAGE_LINKS_1_FIELD = 'global_field_mainpage_links_1_field';
+
+
 }
