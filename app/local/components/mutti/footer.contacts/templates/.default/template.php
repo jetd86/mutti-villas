956be649ecd9f4f4999a6f316f8d2a8e58f65acc
root <root@DESKTOP-2T29VPA.localdomain>
2025-09-21 23:20:26 +0300

    loc class

--- app/local/components/mutti/footer.contacts/templates/.default/template.php
+++ app/local/components/mutti/footer.contacts/templates/.default/template.php
@@ -1,4 +1,6 @@
-<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
+<? use Bitrix\Main\Localization\Loc;
+
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
     die();
 
 /** @var array $arParams */
